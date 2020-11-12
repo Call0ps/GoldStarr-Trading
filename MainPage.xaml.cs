869274807314ade7057486cc59f9bc5aecbef211
DESKTOP-1IB2BBC\Miki <savamichael@hotmail.com>
2020-11-12 21:33:35 +0100

    Code Cleanup

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -23,36 +23,12 @@ namespace GoldStarr_Trading
 		StoreClass store;
 		private App _app;
 
-		public const string MatchEmailPattern =
-			@"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
-	 + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
-				[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
-	 + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?
-				[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-	 + @"([a-zA-Z0-9]+[\w-]+\.)+[a-zA-Z]{1}[a-zA-Z0-9-]{1,23})$";
-
 
 		public MainPage()
 		{
 			this.InitializeComponent();
 			store = new StoreClass();
 			_app = (App)App.Current;
-
-			#region OLD
-			//DataContext = this;
-
-			//CustomerOrders = new ObservableCollection<CustomerOrderClass>();
-
-			//InStockList.ItemsSource = store.GetCurrentStockList();
-			//StockToAddList.ItemsSource = store.GetCurrentDeliverysList();
-			//CustomersTabComboBoxCustomerName.ItemsSource = store.GetCurrentCustomerList();
-			//CustomerView.ItemsSource = store.GetCurrentCustomerList();
-			//CustomerList = new ObservableCollection<CustomerClass>(store.GetCurrentCustomerList());
-
-			//StockList = new ObservableCollection<StockClass>(store.GetCurrentStockList());
-			//CustomerOrders = new ObservableCollection<CustomerOrderClass>(store.GetCurrentCustomerOrdersList());
-			#endregion
-
 		}
 
 
@@ -321,7 +297,6 @@ namespace GoldStarr_Trading
 		{
 			store.TrySendQO();
 		}
-
 
 		private void SuppliersTabComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
